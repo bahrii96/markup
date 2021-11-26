b67d726a387062d64b5b6c2606dc2c1ca34cd8c1
bahrii96 <81860681+bahrii96@users.noreply.github.com>
2021-11-27 01:06:30 +0300

    зробив слайдер

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
-    $('.interest__slider').slick({
-        dots: true,
+    $('.slider__list').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed:1500,
         // кількість елементів
-        slidesToShow: 3,
+       
         // з якого слайду починається
-        initialSlide:3,
+        
         // прокручування безкінечне
-        infinite: false,
+        
         // ділення елемента на частини потрібно при перетягувванні мишкою
         touchThreshold: 10,
         // центрує зображення
-        centerMode:true
+        
     });
 })
